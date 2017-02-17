081ef9c5f607a82be42bb1889bd0ca46e68cc1ab
Shreeshrii <shreeshrii@gmail.com>
2017-02-17 19:32:48 +0530

    Update createcache.sh

--- createcache.sh
+++ createcache.sh
@@ -4,13 +4,15 @@ echo "cache already built, if running these commands, clear cache dirs first"
 
 #   wget -O ./jar/ocrevaluation.jar https://bintray.com/impactocr/maven/download_file?file_path=eu%2Fdigitisation%2FocrevalUAtion%2F1.3.0%2FocrevalUAtion-1.3.0-jar-with-dependencies.jar
 
-   rm -rf imagesbih-save
+   rm -rf imagesbih-sav
+   mkdir imagesbih-sav
    git submodule add https://github.com/Shreeshrii/imagesbih
-   mv /home/travis/build/Shreeshrii/tess4eval/imagesbih imagesbih/imagesbih-save
+   mv /home/travis/build/Shreeshrii/tess4eval/imagesbih imagesbih-save
    
    rm -rf imageshin-save
+   mkdir imageshin-save
    git submodule add https://github.com/Shreeshrii/imageshin
-   mv /home/travis/build/Shreeshrii/tess4eval/imageshin imageshin/imageshin-save
+   mv /home/travis/build/Shreeshrii/tess4eval/imageshin imageshin-save
    
 #   wget -O ./IIIT_Hindi_100.zip  http://ocr.iiit.ac.in/IIIT_Hindi_100.zip
 #   unzip ./IIIT_Hindi_100.zip
