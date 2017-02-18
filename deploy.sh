2b5122a5b463944a7b6440aca11a34c5f9003793
Shreeshrii <shreeshrii@gmail.com>
2017-02-18 09:13:23 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ cd ./out
 cp ./gt_out.html ./index-${SHA}.html
 mv ./gt_out.html ./index.html
 git init
-git add --all .
-git commit -m  "${SHA} - Update Github Pages" 
+git add ./index.html  ./index-${SHA}.html
+git commit -m  "${SHA} - Update https://shreeshrii.github.io/tess4eval/ index Pages" 
 git push  --force https://user:${GH_TOKEN}@github.com/${GITHUB_REPO}.git master:gh-pages
