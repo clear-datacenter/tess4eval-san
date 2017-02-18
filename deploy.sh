c19d23326a2a9c245c9158e8f36279b545769f85
Shreeshrii <shreeshrii@gmail.com>
2017-02-18 15:33:38 +0530

    ${SHA_SHORT}

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd ./out
-cp ./gt_out.html ./index-${SHA}.html
+cp ./gt_out.html ./index-${SHA_SHORT}.html
 mv ./gt_out.html ./index.html
 git init
-git add ./index.html  ./index-${SHA}.html
-git commit -m  "${SHA} - Update https://shreeshrii.github.io/tess4eval/ index Pages" 
+git add ./index.html  ./index-${SHA_SHORT}.html
+git commit -m  "${SHA_SHORT} - Update https://shreeshrii.github.io/tess4eval/ index Pages" 
 git push  --force https://user:${GH_TOKEN}@github.com/${GITHUB_REPO}.git master:gh-pages
