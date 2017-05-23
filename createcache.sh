51f4d243bee8deb82ed9dea8bace40277243fc19
Shreeshrii <shreeshrii@gmail.com>
2017-05-23 20:11:11 +0530

    Update createcache.sh

--- createcache.sh
+++ createcache.sh
@@ -2,9 +2,12 @@
 
 echo "create cache"
 
-rm -rf ./tess4-save
-mkdir ./tess4-save
+rm -rf ./jar
+mkdir ./jar
+wget -O ./jar/ocrevaluation.jar https://bintray.com/impactocr/maven/download_file?file_path=eu%2Fdigitisation%2FocrevalUAtion%2F1.3.0%2FocrevalUAtion-1.3.0-jar-with-dependencies.jar
 
+ rm -rf ./tess4-save
+ mkdir ./tess4-save
  wget -O ./tess4-save/libopenjp2.deb https://launchpad.net/~alex-p/+archive/ubuntu/tesseract-ocr/+files/libopenjp2-7_2.1.2-1+nmu1ppa1~trusty1_amd64.deb
  wget -O ./tess4-save/liblept5.deb https://launchpad.net/~alex-p/+archive/ubuntu/tesseract-ocr/+files/liblept5_1.74.1-1+nmu1ppa1~trusty1_amd64.deb
  wget -O ./tess4-save/tesseract-ocr-eng.deb https://launchpad.net/~alex-p/+archive/ubuntu/tesseract-ocr/+files/tesseract-ocr-eng_4.00~git11-8bf2e7a-2ppa1~trusty1_all.deb
@@ -12,13 +15,11 @@ mkdir ./tess4-save
  wget -O ./tess4-save/tesseract-ocr-hin.deb https://launchpad.net/~alex-p/+archive/ubuntu/tesseract-ocr/+files/tesseract-ocr-hin_4.00~git11-8bf2e7a-2ppa1~trusty1_all.deb
  wget -O ./tess4-save/tesseract-ocr.deb https://launchpad.net/~alex-p/+archive/ubuntu/tesseract-ocr/+files/tesseract-ocr_4.00~git1759-a011b15-1ppa1~trusty1_amd64.deb
  wget -O ./tess4-save/libtesseract4.deb https://launchpad.net/~alex-p/+archive/ubuntu/tesseract-ocr/+files/libtesseract4_4.00~git1987-8bd2fa7-1ppa1~trusty1_amd64.deb
-#   wget -O ./jar/ocrevaluation.jar https://bintray.com/impactocr/maven/download_file?file_path=eu%2Fdigitisation%2FocrevalUAtion%2F1.3.0%2FocrevalUAtion-1.3.0-jar-with-dependencies.jar
+ wget -O ./tess4-save/tesseract-ocr.deb https://launchpad.net/~alex-p/+archive/ubuntu/tesseract-ocr/+files/tesseract-ocr_4.00~git1987-8bd2fa7-1ppa1~trusty1_amd64.deb
 
-rm ./tess4-save/tesseract-ocr.deb
-wget -O ./tess4-save/tesseract-ocr.deb https://launchpad.net/~alex-p/+archive/ubuntu/tesseract-ocr/+files/tesseract-ocr_4.00~git1987-8bd2fa7-1ppa1~trusty1_amd64.deb
-rm -rf ./tess4data-save
-mkdir ./tess4data-save
-wget -O ./tess4data-save/bih.traineddata https://github.com/Shreeshrii/tessdata4alpha/raw/master/bih.traineddata
+ rm -rf ./tess4data-save
+ mkdir ./tess4data-save
+ wget -O ./tess4data-save/bih.traineddata https://github.com/Shreeshrii/tessdata4alpha/raw/master/bih.traineddata
 
    rm -rf imagesbih-save
    mkdir imagesbih-save
