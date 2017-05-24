18d82856a321b6fa921e4616dc3b0993f1920e02
Shreeshrii <shreeshrii@gmail.com>
2017-05-24 23:18:35 +0530

    Update 5runtesseract.sh

--- 5runtesseract.sh
+++ 5runtesseract.sh
@@ -15,3 +15,22 @@ echo "Images from Shreeshrii/imagessan"
         tesseract ${img_file} ${TRAVIS_BUILD_DIR}/${LANGTWO}/${filename} --psm 6 --oem 1 -l $LANGTWO 
     done   
     
+cd ${TRAVIS_BUILD_DIR}/imagessan-save/imagessan/oldstylefontsamples
+echo "Images from Shreeshrii/imagessan/oldstylefontsamples"
+    img_files=$(ls *.png)
+    for img_file in ${img_files}; do
+        filename=$(basename "${img_file##*/}" .png)
+        echo ${img_file}
+        cp ${filename}.txt ${TRAVIS_BUILD_DIR}/gt
+        tesseract ${img_file} ${TRAVIS_BUILD_DIR}/${LANG}/${filename} --psm 6 --oem 1 -l $LANG 
+        tesseract ${img_file} ${TRAVIS_BUILD_DIR}/${LANGTWO}/${filename} --psm 6 --oem 1 -l $LANGTWO 
+    done      
+    img_files=$(ls *.tif)
+    for img_file in ${img_files}; do
+        filename=$(basename "${img_file##*/}" .tif)
+        echo ${img_file}
+        cp ${filename}.txt ${TRAVIS_BUILD_DIR}/gt
+        tesseract ${img_file} ${TRAVIS_BUILD_DIR}/${LANG}/${filename} --psm 6 --oem 1 -l $LANG 
+        tesseract ${img_file} ${TRAVIS_BUILD_DIR}/${LANGTWO}/${filename} --psm 6 --oem 1 -l $LANGTWO 
+    done  
+    
