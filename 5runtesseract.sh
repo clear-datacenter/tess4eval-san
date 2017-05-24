e6f8a7e4d8de3d6f9212c519e8fc88db88a5eee0
Shreeshrii <shreeshrii@gmail.com>
2017-05-24 22:55:42 +0530

    Create 5runtesseract.sh

---
+++ 5runtesseract.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir ${TRAVIS_BUILD_DIR}/${LANG}
+mkdir ${TRAVIS_BUILD_DIR}/${LANGTWO}
+mkdir ${TRAVIS_BUILD_DIR}/gt
+mkdir $TRAVIS_BUILD_DIR/out
+
+cd ${TRAVIS_BUILD_DIR}/imagessan-save/imageshin/groundtruthimages
+echo "Images from Shreeshrii/imagessan"
+    img_files=$(ls *.png)
+    for img_file in ${img_files}; do
+        filename=$(basename "${img_file##*/}" .png)
+        echo ${img_file}
+        cp ${filename}.txt ${TRAVIS_BUILD_DIR}/gt
+        tesseract ${img_file} ${TRAVIS_BUILD_DIR}/${LANG}/${filename} --psm 6 --oem 1 -l $LANG 
+        tesseract ${img_file} ${TRAVIS_BUILD_DIR}/${LANGTWO}/${filename} --psm 6 --oem 1 -l $LANGTWO 
+    done   
+    
