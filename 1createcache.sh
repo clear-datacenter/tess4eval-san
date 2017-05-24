e28f9f66c88a4cec4314d8ec3916a0c80053044f
Shreeshrii <shreeshrii@gmail.com>
2017-05-24 22:51:47 +0530

    imagessan

--- 1createcache.sh
+++ 1createcache.sh
@@ -10,3 +10,8 @@ echo "create tesseract 4 dev cache"
  rm -rf ./tess4data-save
  mkdir ./tess4data-save
  wget -O ./tess4data-save/bih.traineddata https://github.com/Shreeshrii/tessdata4alpha/raw/master/bih.traineddata
+
+   rm -rf imagessan-save
+   mkdir imagessan-save
+   git submodule add https://github.com/Shreeshrii/imagessan
+   mv /home/travis/build/Shreeshrii/tess4eval/imagessan imagessan-save
