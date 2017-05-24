780dfcc230f9c3f95851c07130926986dbc100d5
Shreeshrii <shreeshrii@gmail.com>
2017-05-24 21:15:44 +0530

    Create update-gh-pages.sh

---
+++ update-gh-pages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo -e "Starting to update gh-pages\n"
+
+  #go to home and use git branch setup earlier
+  cd $HOME
+  
+  #go into directory and copy data we're interested in to that directory
+  cd gh-pages
+  cp -Rf $TRAVIS_BUILD_DIR/out/* .
+
+  #add, commit and push files
+  git add -f .
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
+  
+  echo -e "Done magic :-) Updated https://shreeshrii.github.io/tess4eval_marathi/  \n"
+fi
