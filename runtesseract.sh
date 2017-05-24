a2c37e1c50bf21f6657fb227d5d77e2037a8fc1c
Shreeshrii <shreeshrii@gmail.com>
2017-05-24 21:21:13 +0530

    Update runtesseract.sh

--- runtesseract.sh
+++ runtesseract.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mkdir ./out
-mkdir ./ocr
-mkdir ./gt
+mkdir ${TRAVIS_BUILD_DIR}/${LANG}
+mkdir ${TRAVIS_BUILD_DIR}/$LANGTWO}
+mkdir ${TRAVIS_BUILD_DIR}/gt
 
 cd ${TRAVIS_BUILD_DIR}/imageshin-save/imageshin
 echo "Images from Shreeshrii/imageshin"
@@ -11,7 +11,8 @@ echo "Images from Shreeshrii/imageshin"
         filename=$(basename "${img_file##*/}" .png)
         echo ${img_file}
         cp ${filename}.txt ${TRAVIS_BUILD_DIR}/gt
-        tesseract ${img_file} ${TRAVIS_BUILD_DIR}/ocr/${filename} --psm 6 --oem 1 -l $LANG 
+        tesseract ${img_file} ${TRAVIS_BUILD_DIR}/${LANG}/${filename} --psm 6 --oem 1 -l $LANG 
+        tesseract ${img_file} ${TRAVIS_BUILD_DIR}/${LANGTWO}/${filename} --psm 6 --oem 1 -l $LANGTWO 
     done   
     
       
@@ -22,5 +23,6 @@ echo "Images from Shreeshrii/imageshin"
         filename=$(basename "${img_file##*/}" .png)
         echo ${img_file}
         cp ${filename}.txt ${TRAVIS_BUILD_DIR}/gt
-        tesseract  ${img_file}  ${TRAVIS_BUILD_DIR}/ocr/${filename} --psm 6 --oem 1 -l $LANG 
+        tesseract  ${img_file}  ${TRAVIS_BUILD_DIR}/${LANG}/${filename} --psm 6 --oem 1 -l $LANG 
+        tesseract ${img_file} ${TRAVIS_BUILD_DIR}/${LANGTWO}/${filename} --psm 6 --oem 1 -l $LANGTWO 
     done   
