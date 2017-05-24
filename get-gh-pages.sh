4120ca3d3d41175975ce719da2169b41ef99d307
Shreeshrii <shreeshrii@gmail.com>
2017-05-24 21:14:33 +0530

    Create get-gh-pages.sh

---
+++ get-gh-pages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo -e "Starting to get gh-pages\n"
+  
+  mkdir ${TRAVIS_BUILD_DIR}/out
+
+  #go to home and setup git
+  cd $HOME
+
+  #using token clone gh-pages branch
+  git clone --quiet --branch=gh-pages https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git   gh-pages > /dev/null
+
+  #go into directory and copy data we're interested in to that directory
+  cd gh-pages
+  cp -f *.html $TRAVIS_BUILD_DIR/out/
+
+  echo -e "Got *.html from gh-pages\n"
+fi
