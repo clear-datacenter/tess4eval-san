3114e1b09ca0d563faf613854ea584f7413810c0
Shreeshrii <shreeshrii@gmail.com>
2017-05-24 17:37:37 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,4 @@ mv ./gt_out.html ./index.html
 git init
 git add ./index.html  ./index-${DATE}.html
 git commit -m  "${DATE} - Update https://shreeshrii.github.io/tess4eval/ index Pages" 
-git push  --force https://user:${GH_TOKEN}@github.com/${GITHUB_REPO}.git master:gh-pages
+git push  --force https://user:${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git master:gh-pages
