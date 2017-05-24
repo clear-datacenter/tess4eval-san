b34f567e73a0e96cb1dbfcd97b756fa92a479ff8
Shreeshrii <shreeshrii@gmail.com>
2017-05-24 20:26:20 +0530

    Update runtesseract.sh

--- runtesseract.sh
+++ runtesseract.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-LANG="hin"
-
 mkdir ./out
 mkdir ./ocr
 mkdir ./gt
@@ -26,19 +24,3 @@ echo "Images from Shreeshrii/imageshin"
         cp ${filename}.txt ${TRAVIS_BUILD_DIR}/gt
         tesseract  ${img_file}  ${TRAVIS_BUILD_DIR}/ocr/${filename} --psm 6 --oem 1 -l $LANG 
     done   
-
-cd ${TRAVIS_BUILD_DIR}
-java -cp ./jar/ocrevaluation.jar  eu.digitisation.Main -gt ./gt -ocr ./ocr utf8 -d ./out
-
-DATE=$(date +%d/%m/%Y_%H:%M:%S)
-echo ${DATE}
-
-cd ./out
-
-cp ./gt_out.html ./index-$LANG-${DATE}.html
-mv ./gt_out.html ./index.html
-
-git init
-git add ./index.html  ./index-${DATE}.html
-git commit -m  "${DATE} - Update https://shreeshrii.github.io/tess4eval/ index Pages" 
-git push  --force https://user:${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git master:gh-pages
