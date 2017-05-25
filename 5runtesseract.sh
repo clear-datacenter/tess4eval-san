79df7a88f694fada390ffd94f8c26172c0091718
Shreeshrii <shreeshrii@gmail.com>
2017-05-25 21:33:05 +0530

    fix dir name

--- 5runtesseract.sh
+++ 5runtesseract.sh
@@ -34,7 +34,7 @@ img_files=$(ls *.tif)
         tesseract ${img_file} ${TRAVIS_BUILD_DIR}/${LANGTWO}/${filename} --psm 6 --oem 1 -l $LANGTWO 
     done  
     
-cd ${TRAVIS_BUILD_DIR}/imagesdeva-save/images
+cd ${TRAVIS_BUILD_DIR}/imagesdeva-save/tess4eval_deva/images
 echo "Images from Shreeshrii/tess4eval_deva"
 img_files=$(ls *.png)
     for img_file in ${img_files}; do
