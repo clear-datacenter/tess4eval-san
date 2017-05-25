2f3cb8bfb2999bf8a7122549e52f6002ea41ed70
Shreeshrii <shreeshrii@gmail.com>
2017-05-25 21:49:37 +0530

    Update 5runtesseract.sh

--- 5runtesseract.sh
+++ 5runtesseract.sh
@@ -14,10 +14,12 @@ img_files=$(ls *.png)
         tesseract ${img_file} ${TRAVIS_BUILD_DIR}/${LANG}/${filename} --psm 6 --oem 1 -l $LANG 
         tesseract ${img_file} ${TRAVIS_BUILD_DIR}/${LANGTWO}/${filename} --psm 6 --oem 1 -l $LANGTWO 
     done   
-    
+
+# DO NOT PROCESS OLD STYLE IMAGES - ZZZ    
+
 cd ${TRAVIS_BUILD_DIR}/imagessan-save/imagessan/oldstylefontsamples
 echo "Images from Shreeshrii/imagessan/oldstylefontsamples"
-img_files=$(ls *.png)
+img_files=$(ls ZZZ*.png)
     for img_file in ${img_files}; do
         filename=$(basename "${img_file##*/}" .png)
         echo ${img_file}
@@ -25,7 +27,7 @@ img_files=$(ls *.png)
         tesseract ${img_file} ${TRAVIS_BUILD_DIR}/${LANG}/${filename} --psm 6 --oem 1 -l $LANG 
         tesseract ${img_file} ${TRAVIS_BUILD_DIR}/${LANGTWO}/${filename} --psm 6 --oem 1 -l $LANGTWO 
     done      
-img_files=$(ls *.tif)
+img_files=$(ls ZZZ*.tif)
     for img_file in ${img_files}; do
         filename=$(basename "${img_file##*/}" .tif)
         echo ${img_file}
@@ -34,9 +36,11 @@ img_files=$(ls *.tif)
         tesseract ${img_file} ${TRAVIS_BUILD_DIR}/${LANGTWO}/${filename} --psm 6 --oem 1 -l $LANGTWO 
     done  
     
+# DO NOT PROCESS OLD STYLE IMAGES - ZZZ    
+    
 cd ${TRAVIS_BUILD_DIR}/imagesdeva-save/tess4eval_deva/images
 echo "Images from Shreeshrii/tess4eval_deva"
-img_files=$(ls *.png)
+img_files=$(ls ZZZ*.png)
     for img_file in ${img_files}; do
         filename=$(basename "${img_file##*/}" .png)
         echo ${img_file}
