102fe5ec2541e908a979f4f41fdc90e2870f5a3f
Shreeshrii <shreeshrii@gmail.com>
2017-07-02 20:31:43 +0530

    Update 2installtesseract.sh

--- 2installtesseract.sh
+++ 2installtesseract.sh
@@ -25,6 +25,6 @@ sudo cp ./tess4data-save/bih.traineddata /usr/share/tesseract-ocr/4.00/tessdata
 
 # ls /usr/share/tesseract-ocr/4.00/tessdata
 
-# tesseract -v
-# tesseract --list-langs
-# tesseract ./tif/phototest.tif stdout --oem 1 -l eng
+tesseract -v
+tesseract --list-langs
+tesseract ./tif/phototest.tif stdout --oem 1 -l eng
