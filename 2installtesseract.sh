10fefe1df0d448f35b5d54f2fc92a5d6c76eacb1
Shreeshrii <shreeshrii@gmail.com>
2017-07-09 22:54:00 +0530

    Update 2installtesseract.sh

--- 2installtesseract.sh
+++ 2installtesseract.sh
@@ -21,12 +21,10 @@ sudo apt-get install -f
 sudo dpkg -i  ./tess4dev-save/tesseract-ocr.deb 
 sudo apt-get install -f
 
-sudo cp ./tess4data-save/bih.traineddata /usr/share/tesseract-ocr/4.00/tessdata
 sudo cp ./tess4data-save/bihtune.traineddata /usr/share/tesseract-ocr/4.00/tessdata
 sudo cp ./tess4data-save/hinlayer.traineddata /usr/share/tesseract-ocr/4.00/tessdata
 
 # ls /usr/share/tesseract-ocr/4.00/tessdata
-
-tesseract -v
+#tesseract -v
 tesseract --list-langs
-tesseract ./tif/phototest.tif stdout --oem 1 -l eng
+#tesseract ./tif/phototest.tif stdout --oem 1 -l eng
