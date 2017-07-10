625ff67f7694e03ea4bb6a2bab05cfbda9ac7a8b
Shreeshrii <shreeshrii@gmail.com>
2017-07-11 00:49:40 +0530

    Update 6deploy.sh

--- 6deploy.sh
+++ 6deploy.sh
@@ -8,4 +8,8 @@ mv gt_$LANG.html index-$LANG.html
 cp gt_$LANGTWO.html index-$LANGTWO-$TRAVIS_BUILD_NUMBER.html
 mv gt_$LANGTWO.html index-$LANGTWO.html
 
-echo "<html><head><title>Tesseract 4.00.00alpha LSTM Evaluation for Devanagari</title></head><body><h1>Tesseract 4.0 LSTM Evaluation for Devanagari</h1><ul><li><a href=index-$LANGTWO.html>OCRed using $LANGTWO traineddata</a></li><li><a href=index-$LANG.html>OCRed using $LANG traineddata</a></li></ul></body></html>" > index.html
+echo "<html><head><title>Tesseract 4.00.00alpha LSTM Evaluation for Devanagari</title></head>" \
+"<body><h1>Tesseract 4.0 LSTM Evaluation for Devanagari</h1>" \
+"<ul><li><a href=index-$LANGTWO.html>OCRed using $LANGTWO traineddata</a></li>" \
+"<li><a href=index-$LANG.html>OCRed using $LANG traineddata</a></li></ul>" \
+"</body></html>" > index.html
