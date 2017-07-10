ccb1ee59cb25a8027077f975e4c78b5af486c24f
Shreeshrii <shreeshrii@gmail.com>
2017-07-10 08:59:10 +0530

    Update 1createcache.sh

--- 1createcache.sh
+++ 1createcache.sh
@@ -3,4 +3,4 @@
  rm -rf ./tess4data-save
  mkdir ./tess4data-save
  wget -O ./tess4data-save/bihtune.traineddata https://github.com/Shreeshrii/tess4training/raw/gh-pages/san.traineddata
- wget -O ./tess4data-save/hinlayer.traineddata https://github.com/Shreeshrii/tess4train/raw/gh-pages/san.traineddata
+ #wget -O ./tess4data-save/hinlayer.traineddata https://github.com/Shreeshrii/tess4train/raw/gh-pages/san.traineddata
