f36ef2a59394c807efc737cd133aae3cabee2cf2
Shreeshrii <shreeshrii@gmail.com>
2017-07-11 11:02:19 +0530

    *.tif from imagessan

--- 5runtesseract.sh
+++ 5runtesseract.sh
@@ -10,6 +10,14 @@ img_files=$(ls *.png)
         tesseract ${img_file} ${TRAVIS_BUILD_DIR}/${LANG}/${filename} --psm 6 --oem 1 -l $LANG 
         tesseract ${img_file} ${TRAVIS_BUILD_DIR}/${LANGTWO}/${filename} --psm 6 --oem 1 -l $LANGTWO 
     done   
+    img_files=$(ls *.tif)
+    for img_file in ${img_files}; do
+        filename=$(basename "${img_file##*/}" .tif)
+        echo ${img_file}
+        cp ${filename}.txt ${TRAVIS_BUILD_DIR}/gt
+        tesseract ${img_file} ${TRAVIS_BUILD_DIR}/${LANG}/${filename} --psm 6 --oem 1 -l $LANG 
+        tesseract ${img_file} ${TRAVIS_BUILD_DIR}/${LANGTWO}/${filename} --psm 6 --oem 1 -l $LANGTWO 
+    done  
 
 # to NOT PROCESS OLD STYLE IMAGES - change *.png to ZZZ*.png (gudakesa)   and ZZZ*.tif (santipur)
 
