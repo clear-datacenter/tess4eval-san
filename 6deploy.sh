9e5b3bbf72e3e22b5d852eedf40519e100fb05e6
Shreeshrii <shreeshrii@gmail.com>
2017-07-12 19:05:53 +0530

    Update 6deploy.sh

--- 6deploy.sh
+++ 6deploy.sh
@@ -2,14 +2,11 @@
 
 cd $TRAVIS_BUILD_DIR/out
 
-cp gt_$LANG.html index-$LANG-$TRAVIS_BUILD_NUMBER.html
 mv gt_$LANG.html index-$LANG.html
-
-cp gt_$LANGTWO.html index-$LANGTWO-$TRAVIS_BUILD_NUMBER.html
 mv gt_$LANGTWO.html index-$LANGTWO.html
 
-echo "<html><head><title>Tesseract 4.00.00alpha LSTM Evaluation for Devanagari</title></head>" \
-"<body><h1>Tesseract 4.0 LSTM Evaluation for Devanagari</h1>" \
-"<ul><li><a href=index-$LANGTWO.html>OCRed using $LANGTWO traineddata</a></li>" \
-"<li><a href=index-$LANG.html>OCRed using $LANG traineddata</a></li></ul>" \
+echo "<html><head><title>Tesseract 4.00.00alpha LSTM Evaluation for Sanskrit</title></head>" \
+"<body><h2>Tesseract 4.0 LSTM Evaluation for Devanagari</h2>" \
+"<li><a href=index-$LANG.html>OCRed using $LANG traineddata from tessdata repo</a></li></ul>" \
+"<ul><li><a href=index-$LANGTWO.html>OCRed using $LANGTWO traineddata from current training</a></li>" \
 "</body></html>" > index.html
