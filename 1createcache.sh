ba62a51504fae0c552276035f49793459fab78a8
Shreeshrii <shreeshrii@gmail.com>
2017-07-13 15:51:54 +0530

    use bihtune as of travis 499
    https://github.com/Shreeshrii/tess4training/blob/8dc4f8488e74d4a934168844932c8a526d70c1d9/bihtune.traineddata?raw=true

--- 1createcache.sh
+++ 1createcache.sh
@@ -2,6 +2,5 @@
 
  rm -rf ./tess4data-save
  mkdir ./tess4data-save
- wget -O ./tess4data-save/bihtune.traineddata https://github.com/Shreeshrii/tess4training/raw/gh-pages/bihtune.traineddata
- #wget -O ./tess4data-save/hinlayer.traineddata https://github.com/Shreeshrii/tess4train/raw/gh-pages/san.traineddata
+ wget -O ./tess4data-save/bihtune.traineddata https://github.com/Shreeshrii/tess4training/blob/8dc4f8488e74d4a934168844932c8a526d70c1d9/bihtune.traineddata?raw=true
  
