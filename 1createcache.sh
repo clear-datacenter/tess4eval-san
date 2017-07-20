31647879a895d5363a50dfcf7797b2a942879712
Shreeshrii <shreeshrii@gmail.com>
2017-07-20 23:20:13 +0530

    Update 1createcache.sh

--- 1createcache.sh
+++ 1createcache.sh
@@ -2,8 +2,10 @@
 
  rm -rf ./tess4data-save
  mkdir ./tess4data-save
- wget -O ./tess4data-save/bihtune.traineddata \
-         https://github.com/Shreeshrii/tess4training/blob/a09bbe913b25b0e623f1bc267a60b192d8e0ccc6/san.traineddata?raw=true
+ wget -O ./tess4data-save/${LANGNEW}.traineddata \
+         https://github.com/Shreeshrii/tess4training/blob/master/docs/sannew.traineddata?raw=true
+ 
+ ### https://github.com/Shreeshrii/tess4training/blob/a09bbe913b25b0e623f1bc267a60b192d8e0ccc6/san.traineddata?raw=true
  
  ### https://github.com/Shreeshrii/tess4training/blob/8dc4f8488e74d4a934168844932c8a526d70c1d9/bihtune.traineddata?raw=true
  
